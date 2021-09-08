100b313bb97a0f203957a3ca67ca51daa946669a
Wences81 <zhylinpochta@gmail.com>
2021-09-08 23:24:24 +0200

    start2

--- src/App.js
+++ src/App.js
@@ -50,6 +50,21 @@ export class App extends Component {
     this.setState({ filter: e.currentTarget.value });
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const normalizedFilter = this.state.filter.toLocaleLowerCase();
     const activeContacts = this.state.contacts.filter((contact) =>
